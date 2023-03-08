d4af2d2e347c8f3f0e73f441793fe26d6afcdd80
JoaquinC539 <cjoaquinangel@gmail.com>
2023-03-08 11:47:00 -0600

    Add new data to DB when get into the url working

--- src/main/java/com/simple_crud/ezcrud/EzcrudApplication.java
+++ src/main/java/com/simple_crud/ezcrud/EzcrudApplication.java
@@ -2,21 +2,22 @@ package com.simple_crud.ezcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.sql.SQLException;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class EzcrudApplication {
 	@GetMapping("/api/new")
-	public String hello() {
-		String message = "Saludo1";
-		return message;
-	}
-
-	@GetMapping("/api/edit")
-	public String second() {
-		return "spage";
+	public String add() throws SQLException, ClassNotFoundException {
+		MensajeDAO mensajeDAO = new MensajeDAO();
+		Mensaje msm = new Mensaje("Hola desde Spring", "Spring web");
+		int registro = mensajeDAO.insertar(msm);
+		String msg = ("Se inserto " + registro + " registro");
+		return msg;
 	}
 
 	public static void main(String[] args) {
