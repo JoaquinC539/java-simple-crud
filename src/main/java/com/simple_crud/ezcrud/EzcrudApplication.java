cc8b1e80f0646ca1801c0de2a8328c998d084c96
JoaquinC539 <cjoaquinangel@gmail.com>
2023-03-08 11:53:27 -0600

    Adding trough postman works with changed settings

--- src/main/java/com/simple_crud/ezcrud/EzcrudApplication.java
+++ src/main/java/com/simple_crud/ezcrud/EzcrudApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.sql.SQLException;
 import java.util.List;
@@ -11,10 +13,11 @@ import java.util.List;
 @SpringBootApplication
 @RestController
 public class EzcrudApplication {
-	@GetMapping("/api/new")
-	public String add() throws SQLException, ClassNotFoundException {
+	@PostMapping("/api/new")
+	public String add(@RequestParam String message, @RequestParam String author)
+			throws SQLException, ClassNotFoundException {
 		MensajeDAO mensajeDAO = new MensajeDAO();
-		Mensaje msm = new Mensaje("Hola desde Spring", "Spring web");
+		Mensaje msm = new Mensaje(message, author);
 		int registro = mensajeDAO.insertar(msm);
 		String msg = ("Se inserto " + registro + " registro");
 		return msg;
