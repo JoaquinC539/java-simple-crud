debc89dfcd3255728a483a2ef907f5b8cd6b58a9
JoaquinC539 <cjoaquinangel@gmail.com>
2023-03-08 11:11:55 -0600

    Added readme

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>Learning Backend with Java and spring</h1>
+<p>Already need to know Java (servlets optional)</p>
--- src/main/java/com/simple_crud/ezcrud/EzcrudApplication.java
+++ src/main/java/com/simple_crud/ezcrud/EzcrudApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class EzcrudApplication {
 	@GetMapping("/index")
 	public String hello() {
-		return "index";
+		String message = "Saludo1";
+		return message;
 	}
 
 	@GetMapping("/second")
