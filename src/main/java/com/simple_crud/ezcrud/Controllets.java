9a90905f250b1009309f93b0e0d1c456d6d1e967
JoaquinC539 <cjoaquinangel@gmail.com>
2023-03-08 19:56:26 -0600

    Working Method doing correctly for a crud

--- src/main/java/com/simple_crud/ezcrud/Controllets.java
+++ src/main/java/com/simple_crud/ezcrud/Controllets.java
@@ -1,5 +1,17 @@
 package com.simple_crud.ezcrud;
 
-public class Controllets {
+import java.sql.SQLException;
 
+public class Controllets {
+    public String newMessage(String message, String author) throws ClassNotFoundException, SQLException {
+        MensajeDAO mensajeDAO = new MensajeDAO();
+        Mensaje msm = new Mensaje(message, author);
+        int registro = mensajeDAO.insertar(msm);
+        String msg = ("Se inserto " + registro + " registro");
+        return msg;
+    }
+
+    public static String getData() {
+        return null;
+    }
 }
--- src/main/java/com/simple_crud/ezcrud/EzcrudApplication.java
+++ src/main/java/com/simple_crud/ezcrud/EzcrudApplication.java
@@ -3,8 +3,10 @@ package com.simple_crud.ezcrud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,6 +28,30 @@ public class EzcrudApplication {
 		return msg;
 	}
 
+	@GetMapping("api/selectAll")
+	public List<Mensaje> select() throws ClassNotFoundException, SQLException {
+		MensajeDAO mensajeDAO = new MensajeDAO();
+		List<Mensaje> data = mensajeDAO.seleccionar();
+		return data;
+	}
+
+	@PutMapping("/api/edit")
+	public int edit(@RequestParam int id, @RequestParam String message, @RequestParam String author)
+			throws SQLException, ClassNotFoundException {
+		MensajeDAO mensajeDAO = new MensajeDAO();
+		Mensaje msm = new Mensaje(id, message, author);
+		int registry = mensajeDAO.editar(msm);
+		return registry;
+	}
+
+	@DeleteMapping("/api/delete")
+	public int delete(@RequestParam int id) throws ClassNotFoundException, SQLException {
+		MensajeDAO mensajeDAO = new MensajeDAO();
+		Mensaje deleteId = new Mensaje(id);
+		int registry = mensajeDAO.eliminar(deleteId);
+		return registry;
+	}
+
 	@GetMapping("/index")
 	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView();
@@ -36,5 +62,4 @@ public class EzcrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EzcrudApplication.class, args);
 	}
-
 }
