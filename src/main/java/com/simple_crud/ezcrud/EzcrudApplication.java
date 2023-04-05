e23fbf546cb1119552c1c8194d49120a678791e7
JoaquinC539 <cjoaquinangel@gmail.com>
2023-04-04 23:24:30 -0600

    removed unused imports

--- src/main/java/com/simple_crud/ezcrud/EzcrudApplication.java
+++ src/main/java/com/simple_crud/ezcrud/EzcrudApplication.java
@@ -3,16 +3,10 @@ package com.simple_crud.ezcrud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.sql.SQLException;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @Controller
